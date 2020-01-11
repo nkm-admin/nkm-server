df268cc0c0c9cbf4c2d68e4a42be1f89a2410a44
xuanmos <me@xuanmo.xin>
2020-01-11 12:42:25 +0800

    用户增加头像修改

--- controller/personalCenter.js
+++ controller/personalCenter.js
@@ -5,7 +5,7 @@ const errorCode = require('../utils/errorCode')
 const encrypt = require('../utils/encrypt')
 
 const updateInfo = async ctx => {
-  const { displayName, email } = ctx.request.body
+  const { displayName, email, avatar } = ctx.request.body
   if (!displayName || !email) {
     ctx.body = new Response(false, errorCode.fail)
   } else {
@@ -15,7 +15,8 @@ const updateInfo = async ctx => {
         UPDATE nkm_users
           SET
             display_name = '${displayName}',
-            user_email = '${email}'
+            user_email = '${email}',
+            avatar = '${avatar}'
         WHERE id = ${id}
       `)
       ctx.body = new Response(true, errorCode.success)
